ac4d935cd16dda356cb35c97b134cfec5b0c1a85
Moaz Adrian <moazadrian@gmail.com>
2025-05-22 17:14:49 +0500

    SearchBar Fix

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -2,9 +2,10 @@ import HostawayCalendar from './HostawaySearchBar';
 
 export default function SearchBar() {
   return (
-    <div style={{ backgroundColor: '#115e72' }} className="flex flex-col items-center py-6 space-y-4">
-
-      {/* Hostaway Calendar below or inline as needed */}
+    <div
+      style={{ backgroundColor: '#115e72' }}
+      className="fixed top-16 left-0 w-full z-40 flex flex-col items-center py-6 space-y-4 shadow-md"
+    >
       <HostawayCalendar />
     </div>
   );
